f9a723861e51c0ea53ae74310c96892e40b30a64
namachan <namachan999@gmail.com>
2016-03-09 03:21:22 +0900

    a

---
+++ stjp/ttfsr/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace ttfsr
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(12, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(192, 20);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(612, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1 or die";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.BackColor = System.Drawing.SystemColors.Window;
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(210, 12);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(128, 20);
+            this.comboBox2.TabIndex = 2;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.BackColor = System.Drawing.SystemColors.Window;
+            this.comboBox3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(344, 12);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(128, 20);
+            this.comboBox3.TabIndex = 3;
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+            // 
+            // comboBox4
+            // 
+            this.comboBox4.BackColor = System.Drawing.SystemColors.Window;
+            this.comboBox4.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox4.FormattingEnabled = true;
+            this.comboBox4.Location = new System.Drawing.Point(478, 12);
+            this.comboBox4.Name = "comboBox4";
+            this.comboBox4.Size = new System.Drawing.Size(128, 20);
+            this.comboBox4.TabIndex = 4;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 38);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(714, 230);
+            this.textBox1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(735, 280);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox4);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ stjp/ttfsr/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//using System.Drawing.Text;
+using System.Windows.Media;
+
+namespace ttfsr
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.Items.Clear();
+            comboBox2.Items.Clear();
+            comboBox3.Items.Clear();
+            comboBox4.Items.Clear();
+            comboBox1.Items.AddRange(Fonts.SystemTypefaces.Select(x => x.FontFamily.Source).Distinct().OrderBy(x => x).ToArray());
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            comboBox2.Items.Clear();
+            comboBox3.Items.Clear();
+            comboBox4.Items.Clear();
+
+            var cb1 = (string)comboBox1.SelectedItem;
+            var ffs = Fonts.SystemTypefaces.Where(x => x.FontFamily.Source == cb1).Select(x => x.Style.ToString()).Distinct();
+            comboBox2.Items.AddRange(ffs.OrderBy(x => x).ToArray());
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            comboBox3.Items.Clear();
+            comboBox4.Items.Clear();
+
+            var cb1 = (string)comboBox1.SelectedItem;
+            var cb2 = (string)comboBox2.SelectedItem;
+            var ffss = Fonts.SystemTypefaces.Where(x => x.FontFamily.Source == cb1 && x.Style.ToString() == cb2)
+                .Select(x => x.Weight.ToString()).Distinct();
+            comboBox3.Items.AddRange(ffss.OrderBy(x => x).ToArray());
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            comboBox4.Items.Clear();
+
+            var cb1 = (string)comboBox1.SelectedItem;
+            var cb2 = (string)comboBox2.SelectedItem;
+            var cb3 = (string)comboBox3.SelectedItem;
+            var ffssw = Fonts.SystemTypefaces.Where(x => x.FontFamily.Source == cb1 && x.Style.ToString() == cb2 && x.Weight.ToString() == cb3)
+                .Select(x => x.Stretch.ToString()).Distinct();
+            comboBox4.Items.AddRange(ffssw.OrderBy(x => x).ToArray());
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = string.Empty;
+            var cb1 = (string)comboBox1.SelectedItem;
+            var cb2 = (string)comboBox2.SelectedItem;
+            var cb3 = (string)comboBox3.SelectedItem;
+            var cb4 = (string)comboBox4.SelectedItem;
+            var ffssws = Fonts.SystemTypefaces
+                .Where(x => x.FontFamily.Source == cb1)
+                .Where(x => string.IsNullOrEmpty(cb2) || x.Style.ToString() == cb2)
+                .Where(x => string.IsNullOrEmpty(cb3) || x.Weight.ToString() == cb3)
+                .Where(x => string.IsNullOrEmpty(cb4) || x.Stretch.ToString() == cb4);
+
+            // test WTF
+            if (ffssws.Count() != 1)
+            {
+                textBox1.AppendText("WTF");
+                return;
+            }
+
+            Typeface tf = ffssws.Single();
+            GlyphTypeface gtf;
+            // test GTF
+            if (false == tf.TryGetGlyphTypeface(out gtf))
+            {
+                textBox1.AppendText("GTF");
+                return;
+            }
+
+            var iu = gtf.CharacterToGlyphMap.Keys.OrderBy(x => x).ToArray();
+            var hs = new HashSet<string>();
+            int c = iu[0], n = iu[0];
+            Action phs = () =>
+            {
+                if (c != n)
+                    hs.Add(string.Format("{0:X4}-{1:X4}", c, n));
+                else
+                    hs.Add(string.Format("{0:X4}", c));
+            };
+
+            for (int i = 1; i < iu.Length; n = iu[i++])
+            {
+                if (iu[i] != n + 1)
+                {
+                    phs();
+                    c = iu[i];
+                }
+            }
+
+            phs();
+            textBox1.AppendText(string.Join(",", hs));
+        }
+    }
+}
